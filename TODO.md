fb30f31a83d23e034cd9cb57b48f2fe9a3982d78
Mike Moran <mmoran0032@gmail.com>
2015-11-10 15:54:08 -0500

    Can use normal  line endings fine

--- TODO.md
+++ TODO.md
@@ -1,9 +1,6 @@
 TODO
 ====
 
-- fix TCP socket recv (untested on actual power supply, but tested with dummy
-  server emulating responses)
-
 - check `curses` output of values
 
 - include Bar in output display (for voltage and current)
--- include/controller.py
+++ include/controller.py
@@ -26,12 +26,12 @@ class WFcontroller(object):
       sleep(self.refreshTime)
 
   def querySupplies(self):
-    self.negativeOutput = self.model.communicate(0, ">DON?\r")
-    self.positiveOutput = self.model.communicate(1, ">DON?\r")
-    self.negativeV = self.model.communicate(0, ">M0?\r")
-    self.negativeA = self.model.communicate(0, ">M1?\r")
-    self.positiveV = self.model.communicate(1, ">M0?\r")
-    self.positiveA = self.model.communicate(1, ">M1?\r")
+    self.negativeOutput = self.model.communicate(0, ">DON?\n")
+    self.positiveOutput = self.model.communicate(1, ">DON?\n")
+    self.negativeV = self.model.communicate(0, ">M0?\n")
+    self.negativeA = self.model.communicate(0, ">M1?\n")
+    self.positiveV = self.model.communicate(1, ">M0?\n")
+    self.positiveA = self.model.communicate(1, ">M1?\n")
 
   def convertData(self):
     self.data["Neg"]["status"] = self.convertIndicator(self.negativeOutput)
