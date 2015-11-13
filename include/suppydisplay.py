09912aef2ea69de55a0ed9434712566c33c3021e
Mike Moran <mmoran0032@gmail.com>
2015-11-12 20:10:39 -0500

    start breaking out individual supply display

---
+++ include/suppydisplay.py
@@ -0,0 +1,35 @@
+# supplydisplay - provides WFSupplyDisplay to show a single power supply
+
+
+class WFSupplyDisplay(object):
+
+    def __init__(self, name, window, size):
+        self.name = name
+        self.window = window
+        self.size = size
+        self.labels = {}
+        self.readbacks = {}
+        self.controls = {}
+
+    def createView(self):
+        self.window.box()
+        self.createAllLabels()
+        self.createAllReadbacks()
+        self.createAllControls()
+
+    def createAllLabels(self):
+        for key, data in self.labels.items():
+            location = data[0], data[1]
+            text = data[2]
+            formatting = data[3]
+            self.window.addstr(location[0], location[1], text, formatting)
+
+    def createAllReadBacks(self):
+        for key, data in self.readbacks.items():
+            location = data[0], data[1]
+            text = data[2]
+            formatting = data[3]
+            self.window.addstr(location[0], location[1], text, formatting)
+
+    def createAllControls(self):
+        pass
